014e6e9a04ce40987764b0070e25216e7704b44f
kolja.wilcke <kolja.wilcke@zalando.de>
2012-07-20 17:34:18 +0200

    restructure: http://vimcasts.org/episodes/synchronizing-plugins-with-git-submodules-and-pathogen/

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/sh
+
+git submodule add git://github.com/mileszs/ack.vim.git bundle/ack
+git submodule add git://gitorious.org/vim-gnupg/vim-gnupg.git bundle/gnupg
+git submodule add git://github.com/vim-scripts/vcscommand.vim.git bundle/vcscommand
+git submodule add git://github.com/vim-scripts/FuzzyFinder.git bundle/FuzzyFinder
+git submodule add git://github.com/vim-scripts/L9.git bundle/L9
+git submodule add git://github.com/sjbach/lusty.git bundle/lusty
+git submodule add git://github.com/msanders/snipmate.vim.git bundle/snipmate
+git submodule add git://github.com/tomtom/tcomment_vim.git bundle/tcomment
+git submodule add git://github.com/vim-scripts/The-NERD-tree.git bundle/nerdTree
+git submodule add git://github.com/kchmck/vim-coffee-script.git bundle/coffeeScript
+git submodule add git://github.com/digitaltoad/vim-jade.git bundle/jade
+git submodule add git://github.com/groenewege/vim-less bundle/less
+git submodule add git://github.com/tpope/vim-repeat.git bundle/repeat
+git submodule add git://github.com/wavded/vim-stylus.git bundle/stylus
+git submodule add git://github.com/tpope/vim-surround.git bundle/surround
+git submodule add git://github.com/tpope/vim-fugitive.git bundle/fugitive
+git submodule add git://github.com/michalliu/sourcebeautify.vim.git bundle/sourcebeautify
+git submodule add git://github.com/Lokaltog/vim-powerline.git bundle/powerline
+git submodule add git://github.com/vim-scripts/taglist.vim.git bundle/taglist
