b61d5b8c0149a3010679015fa427335f8fee655c
Kolja Wilcke <kolja.wilcke@zalando.de>
2015-12-25 17:06:57 +0100

    vim-plug

--- setup.sh
+++ setup.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-# setup vundle
-echo "setup vundle"
-cd `dirname $0`
-git submodule update --init
-
 # make a backup copy of existing .vimrc
-if [[ -e ~/.vimrc ]] 
-then
-  count=`ls -a ~ | grep vimrc | wc -l`
-  old=~/vimrc_${count}.bak
+if [[ -e ~/.vimrc ]] then
+  now=$(date +"%m-%d-%Y")
+  old=~/vimrc-${now}.bak
   echo "backup old vimrc to $old"
   mv ~/.vimrc $old
 fi
+
+# bootstrap vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "make symbolic link of vimrc"
 ln -s ~/.vim/vimrc ~/.vimrc
 
 # install plugins
-vim +PluginInstall +qall
+vim +PlugInstall
